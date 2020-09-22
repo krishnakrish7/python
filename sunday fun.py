083f715060a6c32b83787ca8e7471c741df4da3e
krishnakrish7 <57554182+krishnakrish7@users.noreply.github.com>
2020-09-22 11:00:05 +0530

    Add files via upload

---
+++ sunday fun.py
@@ -0,0 +1,3 @@
+from datetime import date
+now = date.today()
+print("Today's date is", now)
---
+++ sunday fun1.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+today = datetime.now()
+print("current date and time is ", today)
